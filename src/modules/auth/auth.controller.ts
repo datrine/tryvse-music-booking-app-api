a6a8a0f472750f59c73cc14fbf0a8adfaca7c8cd
datrine <trinitietp@gmail.com>
2025-04-02 07:01:33 +0100

    ft:pg supported

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,11 +2,13 @@ import { Body, Controller, HttpStatus, Post, ValidationPipe } from '@nestjs/comm
 import { AuthService } from './auth.service';
 import { UserSigninRequestBodyDTO } from './dto/request.dto';
 import { UserSigninOKResponseBodyDTO } from './dto/response.dto';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller('api/v1/auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
     @Post("/signin")
+                    @ApiOperation({description:"sign in",summary:"sign in"})
     async signin(@Body(new ValidationPipe({ transform: true })) body: UserSigninRequestBodyDTO) {
         let resData = await this.authService.signin(body)
         let { id, ...rest } = resData
